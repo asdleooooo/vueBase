6ec114cb8eb001a33059df881bb8bdcc4e73654f
yangyibin <13090094280@qq.com>
2021-12-23 21:31:50 +0800

    '提交'

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,54 @@
 <template>
   <div>
-    <MyButton/>
+    <Heade/>
+    <Add/>
+    <List/>
   </div>
 </template>
 
 <script>
-import MyButton from "@/components/MyButton.vue"
+import Header from "@/components/Header.vue";
+import Add from  "@/components/Add.vue";
+import List from "@/components/List.vue";
 export default {
   components:{
-    MyButton,
+    Header,
+    Add,
+    List
   }
 }
 </script>
 
 <style>
+  .reply {
+  margin-top: 0px;
+  }
 
+  li {
+    transition: .5s;
+    overflow: hidden;
+  }
+
+  .handle {
+    width: 40px;
+    border: 1px solid #ccc;
+    background: #fff;
+    position: absolute;
+    right: 10px;
+    top: 1px;
+    text-align: center;
+  }
+
+  .handle a {
+    display: block;
+    text-decoration: none;
+  }
+
+  .list-group-item .centence {
+    padding: 0px 50px;
+  }
+
+  .user {
+    font-size: 22px;
+  }
 </style>
